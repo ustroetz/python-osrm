b3e9462caf00f85ea7e6d03f09eab77bb8a831cb
Ulrich Stroetz <ustroetz@gmail.com>
2015-04-20 17:51:26 +0200

    change directory structure for setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='forestcost',
+    name='osrm',
     version='0.1',
     author="Ulric Stroetz",
     author_email="ustroetz@gmail.com",
-    packages=['python-osrm'],
+    package_dir={'': 'src'},
+    packages=['osrm'],
     install_requires=[
       'requests',
       'polyline'],
