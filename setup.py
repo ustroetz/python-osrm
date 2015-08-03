3e5542feac759d662ce762498bef5da238b6235e
mthh <mthh@users.noreply.github.com>
2015-08-03 15:53:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup
 
 setup(
     name='osrm',
-    version='0.1',
-    author="Ulric Stroetz",
+    version='0.11',
+    author="Ulric Stroetz, mthh",
     author_email="ustroetz@gmail.com",
     package_dir={'': 'src'},
     packages=['osrm'],
     install_requires=[
       'requests',
-      'polyline'],
+      'polyline',
+      'GDAL',
+      'numpy',
+      'pandas']
       )
