a0b13be6598285aba9dd6df58c3bef004196b5cf
mthh <mthh@users.noreply.github.com>
2015-10-12 20:07:28 +0200

    Update __init__.py

--- osrm/__init__.py
+++ osrm/__init__.py
@@ -10,4 +10,7 @@ using OSRM 'table' function.
 Allow geometry decoding for 'viaroute' and 'match' functions.
 """
 
-from osrm.core import match, simple_viaroute, locate, nearest, table, table_OD
+from osrm.core import (
+    match, simple_viaroute, locate, nearest, table, table_OD, get_error_id,
+    light_table
+    )
