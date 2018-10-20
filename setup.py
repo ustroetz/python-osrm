9a226b1e0f375b4586647cdc9c84923370fb2786
ustroetz <ustroetz@gmail.com>
2018-10-20 18:24:12 +0200

    Add url to project

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open("requirements.txt") as f:
     requirements = f.read().split('\n')
 
 setup(
-    author="Ulric Stroetz, mthh",
     author_email="ustroetz@gmail.com",
-    description="A Python wrapper around the OSRM API"
-    install_requires=requirements
+    author="Ulric Stroetz, mthh",
+    description="A Python wrapper around the OSRM API",
+    install_requires=requirements,
     name='osrm',
     packages=['osrm'],
     test_suite="tests",
-    version='0.11.2',
+    url="https://github.com/ustroetz/python-osrm",
+    version='0.11.2'
 )
