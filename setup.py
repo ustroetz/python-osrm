430f015e668be3b88896f0d310671d1204d91e46
ustroetz <ustroetz@gmail.com>
2018-10-20 18:19:45 +0200

    Bump version for new pypi release

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ with open("requirements.txt") as f:
 
 setup(
     name='osrm',
-    version='0.11.1',
+    version='0.11.2',
     author="Ulric Stroetz, mthh",
     author_email="ustroetz@gmail.com",
     packages=['osrm'],
     test_suite="tests",
     install_requires=requirements
-    )
+)
